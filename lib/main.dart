cae6a4b5a6564cd259100f8226cc763414189ff0
Leonidas Yopan <leonidasyopan@gmail.com>
2020-09-01 11:35:17 -0300

    WIP personal profile page

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: Home(),
   ));
 }
@@ -10,8 +11,102 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
-        title: Text('Profile'),
+        title: Text('Leonidas Yopan Profile'),
+        backgroundColor: Colors.grey[850],
+        centerTitle: true,
+        elevation: 0.0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0 ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'NAME',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 10.0,),
+            Text(
+              'Leonidas Yopan',
+              style: TextStyle(
+                color: Colors.amberAccent,
+                letterSpacing: 2.0,
+                fontSize: 30.0,
+              ),
+            ),
+            SizedBox(height: 30.0,),
+            Row(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'BIRTH DATE',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        letterSpacing: 2.0,
+                      ),
+                    ),
+                    SizedBox(height: 10.0,),
+                    Text(
+                      'Aug 10th 1985',
+                      style: TextStyle(
+                        color: Colors.amberAccent,
+                        letterSpacing: 2.0,
+                        fontSize: 30.0,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(width: 70.0,),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'AGE',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        letterSpacing: 2.0,
+                      ),
+                    ),
+                    SizedBox(height: 10.0,),
+                    Text(
+                      '35',
+                      style: TextStyle(
+                        color: Colors.amberAccent,
+                        letterSpacing: 2.0,
+                        fontSize: 30.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(height: 30.0),
+            Text(
+              'PROFESSION',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 10.0,),
+            Text(
+              'Fullstack Developer',
+              style: TextStyle(
+                color: Colors.amberAccent,
+                letterSpacing: 2.0,
+                fontSize: 30.0,
+              ),
+            ),
+            SizedBox(height: 30.0,),
+          ],
+        ),
       ),
     );
   }
